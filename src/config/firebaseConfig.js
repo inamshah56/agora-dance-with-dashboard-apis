b642610089201b4a9d56e39977f555f174d87274
Moin <moinbukhari147>
2024-08-21 19:39:28 +0500

    fixes: Secrect keys of Oauth and adminsdk 2

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,14 +1,15 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = await import('../../oauth-notify-firebase-adminsdk-1z1l7-ef62a9518a.json', {
+
+const filePath = process.env.AGORA_ADMIN_SDK;
+const serviceAccount = await import(filePath, {
     assert: { type: 'json' }
 });
 admin.initializeApp({
     // un comment the below line during deployment after setting the 
     // GOOGLE_APPLICATION_CREDENTIALS that contains the path of the service account key
     // credential: admin.credential.applicationDefault(),
-    credential: admin.credential.cert(serviceAccount.default),
-    databaseURL: "https://oauth-notify.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount.default)
 });
 
 export default admin.messaging();
--- src/strategies/googleStrategy.js
+++ src/strategies/googleStrategy.js
@@ -2,8 +2,8 @@ import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import passport from 'passport';
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID_WEB_FRB,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET_WEB_FRB,
+    clientID: process.env.GOOGLE_CLIENT_MOIN,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET_MOIN,
     callbackURL: "http://localhost:8000/api/auth/google/redirect"
 },
     function (accessToken, refreshToken, profile, cb) {
